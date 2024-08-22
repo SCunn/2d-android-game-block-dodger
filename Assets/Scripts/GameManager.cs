3461ca4db4010546e6ae1c22bb7eb64e01fdc366
SCunn <43576829+SCunn@users.noreply.github.com>
2024-08-23 00:38:00 +0100

    game manager and spawning blocks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject block;
+    public float max_X;
+    public Transform spawnPoint;
+    public float spawnRate;
+
+
+    bool gameStarted = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && !gameStarted)
+        {
+            gameStarted = true;
+            StartSpawning();
+        }
+    }
+
+    private void StartSpawning()
+    {
+        InvokeRepeating("SpawnBlock", 0.1f, spawnRate);
+    }
+
+    private void SpawnBlock()
+    {
+        Vector3 spawnPos = spawnPoint.position;
+
+        spawnPos.x = Random.Range(-max_X, max_X);
+
+        Instantiate(block, spawnPos, Quaternion.identity);
     }
 }
